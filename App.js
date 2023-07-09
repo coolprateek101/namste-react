70e1295760d4fb7290f1dc968cfad9fe7043b09c
Prateek <Prateek@themediaant.com>
2023-07-09 12:19:04 +0530

    chnages

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
-{/* <div id = "parent">
-	<div id= "child">
-		<h1>I am h1 tag</h1>
-		<h2>I am h2 tag</h2>
-	</div>
-	<div id="child2">
-		<h1>I am h1 tag</h1>
-		<h2>I am h2 tag</h2>
-	</div>
-</div> */}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 let parent = React.createElement("div", { id: "parent"}, [
 	React.createElement("div", {id: "child"}, [
-		React.createElement("h1", {}, "I am h1 tag"),
-		React.createElement("h2", {}, "I am h2 tag"),
+		React.createElement("h1", {}, "This is Namste React 🚀 😆"),
+		React.createElement("h2", {}, "By Prateek Pandey"),
 	]),
 	React.createElement("div", { id: "child2" }, [
 		React.createElement("h1", {}, "I am h1 tag"),
@@ -21,4 +13,4 @@ let parent = React.createElement("div", { id: "parent"}, [
 ]);
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent) 
+root.render(parent)  
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Namste React# namste-react
+# Namatse React 🚀
+
+#Parcel
+Dev Build
+Local server
+HMR = Hot Module replacement
+File watching Algorithm-- written in c++
+Cashing -Faster Build experience
+Image Optimization
+Minification
+Bundling
+Compress
+Consistent Hashing
+Code splitting
+Differential Bundling - support older browser
